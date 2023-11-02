512c70aba3ef212e4ee78ff147d89cd37c5d5ea7
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-11-02 16:09:51 +0200

    Added Minimum

--- main.cpp
+++ main.cpp
@@ -10,10 +10,18 @@ int getSum(std::vector<int> & numbers){
     }
     return sum;
 }
+int getMin(std::vector<int> & numbers){
+    int mn = INT32_MAX;
+    for(auto & u : numbers){
+        if(mn > u ) mn = u;
+    } 
+    return mn;
+}
 
 int main(int argc, char **argv){
 
     std::vector<int> vector_example = {1,2,3,4,5,6,7,7,9,10} ; 
     std:: cout << getSum(vector_example) << std::endl; 
+    std:: cout << getMin(vector_example) << std::endl; 
     
 }
